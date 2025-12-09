5ca33ba26c57212225eeaa57a04bafa7041fe06e
dilorenzomercedes-coder <dilorenzomercedes@gmail.com>
2025-12-09 10:45:07 -0300

    Fix indentation in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Paleo-tracker/',
   plugins: [react()],
+    base: '/Paleo-tracker/',
 })
